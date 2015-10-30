13f81316dd605ea68b512ea4155f6a9cb8e17971
JP Miller <legendzing@gmail.com>
2015-10-30 10:31:39 -0500

    chore: added more Movie classes

--- entertainment_center.py
+++ entertainment_center.py
@@ -20,16 +20,30 @@ toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to
 # print(toy_story.storyline)
 
 avatar = media.Movie("Avatar", "A marine on an alien planet",
-					 "http://upload.wikimedia.org/wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+					 "http://www.pembinatrails.ca/vincentmassey/pd/photography21G/photoshop/fatma/schew-photoshop%20-%20Unit%208%20Avatar_files/avatar-movie-poster-english1.jpg",
 					 "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 # print(avatar.storyline)
 # avatar.show_trailer()
 
 peanuts_movie = media.Movie("The Peanuts Movie", "A story about a dog and his boy",
-							"https://en.wikipedia.org/wiki/The_Peanuts_Movie#/media/File:Peanuts_2015.jpg",
+							"http://www.impawards.com/2015/posters/snoopy_and_charlie_brown_the_peanuts_movie_ver26.jpg",
 							"https://www.youtube.com/watch?v=fVR4E6Q6u5g")
 # print(peanuts_movie.storyline)
 # peanuts_movie.show_trailer()
 
-movies = [toy_story, avatar, peanuts_movie]
+ex_machina = media.Movie("Ex Machina", "A young coder meets his CEO and participates in a bizarre experiment",
+						 "http://a1.mzstatic.com/us/r30/Video3/v4/f3/fb/9b/f3fb9b48-d3b3-be47-3554-aababef6c654/poster600x600.jpeg",
+						 "https://www.youtube.com/watch?v=bggUmgeMCdc")
+
+theory_of_everything = media.Movie("The Theory of Everything", "The story of Stephen and Jane Hawking",
+									"http://t0.gstatic.com/images?q=tbn:ANd9GcSgW7hpezP5xtikV7WqwwqFm37kqMeJeFEGpYcO30CDcchn9g8m",
+									"https://www.youtube.com/watch?v=Salz7uGp72c")
+
+gravity = media.Movie("Gravity", "Astronauts are stranded in space after the mid-orbit destruction of their space shuttle",
+						"http://t1.gstatic.com/images?q=tbn:ANd9GcRu6dxMqi3cxDsmH1ajpW3Kr-bKPh2Nf57GXRFW4ybrEgcaUu-C",
+						"https://www.youtube.com/watch?v=OiTiKOy59o4")
+
+movies = [toy_story, avatar, peanuts_movie, ex_machina, theory_of_everything,
+		gravity]
+		
 fresh_tomatoes.open_movies_page(movies)
